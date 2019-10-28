706d226472861ba8fdb7ea68cf4f8d346a95b01c
Damini varu <daminivaru@Daminis-MacBook-Pro.local>
2019-10-28 09:04:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code here!
+// let element = document.createElement('div')
+
+// document.body.appendChild(element)
+
+// let ul = document.createElement('ul')
+ 
+// for (let i = 0; i < 3; i++) {
+//   let li = document.createElement('li')
+//   li.innerHTML = (i + 1).toString()
+//   ul.appendChild(li)
+// }
+ 
+// element.appendChild(ul)
+document.querySelector("main#main").remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Damini is the champion!";
